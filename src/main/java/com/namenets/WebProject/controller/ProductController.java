aadb3cec09fa2faca01b4af4e2db0f4c40a52bf4
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-21 09:11:01 +0200

    received data from the client to the server using @RequestBody

--- src/main/java/com/namenets/WebProject/controller/ProductController.java
+++ src/main/java/com/namenets/WebProject/controller/ProductController.java
@@ -4,22 +4,25 @@ package com.namenets.WebProject.controller;
 import com.namenets.WebProject.model.Product;
 import com.namenets.WebProject.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
 public class ProductController {
     @Autowired
     ProductService service;
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts(){
         return service.getProducts();
     }
-    @RequestMapping("/products/{prodId}")
+    @GetMapping("/products/{prodId}")
     public Product getProdById(@PathVariable int prodId){
         return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod){
+        System.out.println(prod);
+        service.addProduct(prod);
     }
 }
--- src/main/java/com/namenets/WebProject/service/ProductService.java
+++ src/main/java/com/namenets/WebProject/service/ProductService.java
@@ -3,19 +3,23 @@ package com.namenets.WebProject.service;
 import com.namenets.WebProject.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
     // for dummy products list
-    List<Product> products=Arrays.asList(new Product(101, "Iphone", 50000), new Product(102, "Canon Camera", 70000), new Product(103, "Shure Mic", 10000));
+    List<Product> products=new ArrayList<>( Arrays.asList(new Product(101, "Iphone", 50000), new Product(102, "Canon Camera", 70000), new Product(103, "Shure Mic", 10000)));
     public List<Product> getProducts(){
         return products;
     }
 
     public Product getProductById(int prodId) {
-        return products.stream().filter(p -> p.getProdId()==prodId).findFirst().get();
+        return products.stream().filter(p -> p.getProdId()==prodId).findFirst().orElse(new Product(100, "No Item", 0));
     }
 
+    public void addProduct(Product prod) {
+        products.add(prod);
+    }
 }
